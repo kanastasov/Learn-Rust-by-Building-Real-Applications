60f30e35072a0131e5d2b8de8b8c18da1e3daf24
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-06-13 17:27:56 +0200

    functions

---
+++ mars_calc/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+
+    calculate_weight_on_mars(100.0);
+}
+
+
+fn calculate_weight_on_mars(weight: f32) -> f32{
+    return 50.0;
+}
---
+++ mars_calc/target/debug/mars_calc.d
@@ -0,0 +1,1 @@
+C:\Users\kiril\Desktop\Learn\ Rust\ by\ Building\ Real\ Applications\mars_calc\target\debug\mars_calc.exe: C:\Users\kiril\Desktop\Learn\ Rust\ by\ Building\ Real\ Applications\mars_calc\src\main.rs
