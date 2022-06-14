6964434d6344cedce034165d79218c6eae596d4e
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-06-14 07:56:28 +0200

    ownership

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input);
     let mut mars_weight: f32 = calculate_weight_on_mars(100.0);
-    mars_weight = mars_weight * 1000.0;
+    //mars_weight = mars_weight * 1000.0;
     println!("Weight on Mars: {}kg", mars_weight ); 
 }
 
