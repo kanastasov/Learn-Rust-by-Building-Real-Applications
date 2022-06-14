e4874ddda631833179b05643b4247d9a9fffe55c
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-06-14 08:16:32 +0200

    finishing touches

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -2,35 +2,41 @@ use std::io;
 
 
 fn main() {
+    println!("Enter your weight (kg):");
     let mut input = String::new();
     // let s1 = &input;
     // let s2 = &input;
     // println!("{} {}", s1,s2);
      
     // some_fn(&mut input);
-    io::stdin().read_line(&mut input);
+    io::stdin().read_line(&mut input).unwrap();
 
-    borrow_string(&input);
-    own_string(input);
+    let weight: f32 = input.trim().parse().unwrap();
+
+    dbg!(weight);
 
 
-    // let mars_weight: f32 = calculate_weight_on_mars(100.0);
+    // borrow_string(&input);
+    // own_string(input);
+
+
+    let mars_weight: f32 = calculate_weight_on_mars(weight);
     //mars_weight = mars_weight * 1000.0;
-    // println!("Weight on Mars: {}kg", mars_weight ); 
+    println!("Weight on Mars: {}kg", mars_weight ); 
 }
 
-fn borrow_string(s: &String){
-    println!("{}",s)
-}
+// fn borrow_string(s: &String){
+//     println!("{}",s)
+// }
 
-fn own_string(s: String){
-    println("{}",s)
-}
+// fn own_string(s: String){
+//     println("{}",s)
+// }
 
 fn calculate_weight_on_mars(weight: f32) -> f32{
     return (weight / 9.81) * 3.711;
 }
 
-fn some_fn(s: &mut String){
+// fn some_fn(s: &mut String){
 
-}
+// }
