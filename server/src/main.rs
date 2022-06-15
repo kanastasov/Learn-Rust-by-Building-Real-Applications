0b5844718ae1f32972dccf858bd69c275d93bcd3
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-06-15 07:32:51 +0200

    struts

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+
+    let server = Server::new("127.0.01.1:8080");
+
+    server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+
+impl Server {
+    fn new(addr: String) -> Self{
+        Self{
+            addr
+        }
+    }
+
+    fn run(&mut self) {
+        
+    }
 }
