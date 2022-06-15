94b32874711fde45c6d4b10229b7aa47cbf2407d
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-06-15 07:23:33 +0200

    starting the server

---
+++ server/target/debug/deps/server-d041c0c22ffaff07.d
@@ -0,0 +1,5 @@
+C:\Users\kiril\Desktop\Learn Rust by Building Real Applications\server\target\debug\deps\server-d041c0c22ffaff07.exe: src\main.rs
+
+C:\Users\kiril\Desktop\Learn Rust by Building Real Applications\server\target\debug\deps\server-d041c0c22ffaff07.d: src\main.rs
+
+src\main.rs:
---
+++ server/target/debug/server.d
@@ -0,0 +1,1 @@
+C:\Users\kiril\Desktop\Learn\ Rust\ by\ Building\ Real\ Applications\server\target\debug\server.exe: C:\Users\kiril\Desktop\Learn\ Rust\ by\ Building\ Real\ Applications\server\src\main.rs
