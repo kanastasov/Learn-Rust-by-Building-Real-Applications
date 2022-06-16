d003c5229eb30afd58a43669489fa54a88ac0c15
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-06-16 12:54:03 +0200

    strings

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,18 @@
 fn main() {
 
-    let server = Server::new("127.0.01.1:8080");
+    // let string = String::from("127.0.0.1:8080");
+    // let string_slice = &string[10..14];
+    // let string_borrow: &str = &string;
+    // let string_literal = "1234";
 
+
+    // dbg!(&string);
+    // dbg!(&string_slice);
+    
+    // dbg!(&string_borrow);
+    // dbg!(&string_literal);
+
+    let server = Server::new("127.0.01.1:8080".to_string());
     server.run();
 }
 
@@ -18,6 +29,6 @@ impl Server {
     }
 
     fn run(&mut self) {
-        
+        println!("Listening on {}", self.addr);
     }
 }
