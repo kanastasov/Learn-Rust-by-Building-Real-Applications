cf3b9e3bd2d86b4958974c2cc00e51297fd10cc1
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-06-22 10:22:44 +0200

    routing incoming requests

--- server/src/website_handler.rs
+++ server/src/website_handler.rs
@@ -1,9 +1,18 @@
 use super::server::Handler;
-use super::http::{Request,Response,StatusCode};
+use super::http::{Request,Response,StatusCode, Method};
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler{
     fn handler_request(&mut slef, request:&Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1>Test</h1>".to_string()))
+        //Response::new(StatusCode::Ok, Some("<h1>Test</h1>".to_string()))
+        match request.method(){
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome here</h1>".to_string())),
+                "/hello" => Response::new(StatusCode::Ok, Some("<h1>hello</h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound,None),
+
+            }
+            _ => Response::new(StatusCode::NotFound,None)
+        }
     }
 }
