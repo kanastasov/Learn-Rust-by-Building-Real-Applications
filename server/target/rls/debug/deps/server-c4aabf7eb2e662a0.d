7a21f33713a908df88783ade62eab3b5b3208bde
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-06-22 08:32:26 +0200

    modelling the http response

--- server/target/rls/debug/deps/server-c4aabf7eb2e662a0.d
+++ server/target/rls/debug/deps/server-c4aabf7eb2e662a0.d
@@ -1,10 +1,11 @@
-c:\Users\kiril\Desktop\Learn Rust by Building Real Applications\server\target\rls\debug\deps\server-c4aabf7eb2e662a0.rmeta: src\main.rs src\http\mod.rs src\http\request.rs src\http\method.rs src\http\query_string.rs src\server.rs
+c:\Users\kiril\Desktop\Learn Rust by Building Real Applications\server\target\rls\debug\deps\server-c4aabf7eb2e662a0.rmeta: src\main.rs src\http\mod.rs src\http\request.rs src\http\method.rs src\http\query_string.rs src\http\response.rs src\server.rs
 
-c:\Users\kiril\Desktop\Learn Rust by Building Real Applications\server\target\rls\debug\deps\server-c4aabf7eb2e662a0.d: src\main.rs src\http\mod.rs src\http\request.rs src\http\method.rs src\http\query_string.rs src\server.rs
+c:\Users\kiril\Desktop\Learn Rust by Building Real Applications\server\target\rls\debug\deps\server-c4aabf7eb2e662a0.d: src\main.rs src\http\mod.rs src\http\request.rs src\http\method.rs src\http\query_string.rs src\http\response.rs src\server.rs
 
 src\main.rs:
 src\http\mod.rs:
 src\http\request.rs:
 src\http\method.rs:
 src\http\query_string.rs:
+src\http\response.rs:
 src\server.rs:
--- server/target/rls/debug/deps/server-c71089f458135566.d
+++ server/target/rls/debug/deps/server-c71089f458135566.d
@@ -1,10 +1,11 @@
-c:\Users\kiril\Desktop\Learn Rust by Building Real Applications\server\target\rls\debug\deps\server-c71089f458135566.rmeta: src\main.rs src\http\mod.rs src\http\request.rs src\http\method.rs src\http\query_string.rs src\server.rs
+c:\Users\kiril\Desktop\Learn Rust by Building Real Applications\server\target\rls\debug\deps\server-c71089f458135566.rmeta: src\main.rs src\http\mod.rs src\http\request.rs src\http\method.rs src\http\query_string.rs src\http\response.rs src\server.rs
 
-c:\Users\kiril\Desktop\Learn Rust by Building Real Applications\server\target\rls\debug\deps\server-c71089f458135566.d: src\main.rs src\http\mod.rs src\http\request.rs src\http\method.rs src\http\query_string.rs src\server.rs
+c:\Users\kiril\Desktop\Learn Rust by Building Real Applications\server\target\rls\debug\deps\server-c71089f458135566.d: src\main.rs src\http\mod.rs src\http\request.rs src\http\method.rs src\http\query_string.rs src\http\response.rs src\server.rs
 
 src\main.rs:
 src\http\mod.rs:
 src\http\request.rs:
 src\http\method.rs:
 src\http\query_string.rs:
+src\http\response.rs:
 src\server.rs:
