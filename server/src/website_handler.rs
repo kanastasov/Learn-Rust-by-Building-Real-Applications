d8409ded5fae27736d3a0b86e2a4b21b1658d85d
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-06-22 10:34:27 +0200

    serving html files

--- server/src/website_handler.rs
+++ server/src/website_handler.rs
@@ -10,14 +10,19 @@ impl WebsiteHandler{
     pub fn new (public_path: String) -> Self {
         Self{public_path}
     }
+
+    fn read_file(&slef, file_path:&str) -> Option<string>{
+        let path = format!("{}/{}",slef.public_path, file_path);
+        fs::read_to_string(path).ok();
+    }
 }
 impl Handler for WebsiteHandler{
     fn handler_request(&mut slef, request:&Request) -> Response {
         //Response::new(StatusCode::Ok, Some("<h1>Test</h1>".to_string()))
         match request.method(){
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome here</h1>".to_string())),
-                "/hello" => Response::new(StatusCode::Ok, Some("<h1>hello</h1>".to_string())),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
                 _ => Response::new(StatusCode::NotFound,None),
 
             }
